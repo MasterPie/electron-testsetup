801cdab169329035306495d4bf6707e5efab03a2
Vivek Pai <vpai@tableau.com>
2015-12-17 13:56:26 -0800

    Initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,18 @@
+// get the dependencies
+var gulp = require('gulp'),
+  childProcess = require('child_process'),
+  electron = require('electron-prebuilt');
+
+var ts = require('gulp-typescript');
+var tsProject = ts.createProject('tsconfig.json');
+
+// create the gulp task
+gulp.task('run', function () {
+    childProcess.spawn(electron, ['./app'], { stdio: 'inherit' });
+});
+ 
+gulp.task('ts', function(done) {
+  var tsResult = tsProject.src()
+    .pipe(ts(tsProject), undefined, ts.reporter.fullReporter());
+  return tsResult.js.pipe(gulp.dest('app/js'));
+});
